cd8180740e46c13a7da741aba4500d4dc4e9ac95
Betsy <betrakiss@hotmail.com>
2015-05-31 15:40:59 +0300

    Added jquery and bootstrap. Successfully getting data from IMDB.

--- app/lib/app.pm
+++ app/lib/app.pm
@@ -1,10 +1,21 @@
 package app;
+
+use v5.012;
+use strict;
+use warnings;
+
 use Dancer ':syntax';
+use IMDB::Film;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+post '/movie' => sub {
+	my $film = new IMDB::Film(crit => params->{movieName});
+	template 'film', { code => $film->code() };
 };
 
 true;
